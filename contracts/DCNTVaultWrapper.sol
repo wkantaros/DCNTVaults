1b88e0a28ef93b5d13f47229c56f90b99d720216
Will Kantaros <wkantaros@gmail.com>
2022-06-02 11:55:13 -0400

    enumerable -> 721s, gas improvements

--- contracts/DCNTVaultWrapper.sol
+++ contracts/DCNTVaultWrapper.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 /// ============ Imports ============
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
@@ -14,12 +14,14 @@ contract DCNTVaultWrapper is Ownable {
 
   /// ============ Immutable storage ============
 
-  /// @notice vault token to be distributed to token holders
-  IERC20 public immutable vaultDistributionToken;
-  /// @notice "ticket" token held by user
-  IERC721Enumerable public immutable nftVaultKey;
+  /// @notice vault token to be distributed to token holders (ierc20)
+  address public immutable vaultDistributionToken;
+  /// @notice "ticket" token held by user (ierc721)
+  address public immutable nftVaultKey;
   /// @notice unlock date when distribution can start happening
   uint256 public immutable unlockDate;
+  /// @notice total supply of nftvaultkey
+  uint256 public immutable totalKeySupply;
   
 
   /// ============ Mutable storage ============
@@ -46,18 +48,20 @@ contract DCNTVaultWrapper is Ownable {
   constructor(
     address _vaultDistributionTokenAddress, 
     address _nftVaultKeyAddress,
-    uint256 _unlockDate
+    uint256 _unlockDate,
+    uint256 _totalKeySupply
   ) {
-    vaultDistributionToken = IERC20(_vaultDistributionTokenAddress);
-    nftVaultKey = IERC721Enumerable(_nftVaultKeyAddress);
+    vaultDistributionToken = _vaultDistributionTokenAddress;
+    nftVaultKey = _nftVaultKeyAddress;
     unlockDate = _unlockDate;
+    totalKeySupply = _totalKeySupply;
   }
 
   /// ============ Functions ============
 
   // returns balance of vault
   function vaultBalance() public view returns (uint256) {
-    return vaultDistributionToken.balanceOf(address(this));
+    return IERC20(vaultDistributionToken).balanceOf(address(this));
   }
 
   // returns total # of tokens already released from vault
@@ -67,20 +71,20 @@ contract DCNTVaultWrapper is Ownable {
 
   // (total vault balance) * (nfts_owned/total_nfts)
   function _pendingPayment(uint256 numNftVaultKeys, uint256 totalReceived) private view returns (uint256) {
-    return (totalReceived * numNftVaultKeys) / nftVaultKey.totalSupply();
+    return (totalReceived * numNftVaultKeys) / totalKeySupply;
   }
 
   // claim all the tokens for a key
   function claim(address to, uint256 tokenId) external {
     require(block.timestamp >= unlockDate, "vault is still locked");
     require(vaultBalance() > 0, "vault is empty");
-    require(nftVaultKey.ownerOf(tokenId) == to, "address does not own token");
+    require(IERC721(nftVaultKey).ownerOf(tokenId) == to, "address does not own token");
     require(!hasClaimedTokenId[tokenId], "token already claimed");
 
     // mark it claimed and send token
     hasClaimedTokenId[tokenId] = true;
     uint256 amount = _pendingPayment(1, vaultBalance() + totalReleased());
-    require(vaultDistributionToken.transfer(to, amount), "Transfer failed");
+    require(IERC20(vaultDistributionToken).transfer(to, amount), "Transfer failed");
     _totalReleased += amount;
     emit Claimed(to, amount);
   }
